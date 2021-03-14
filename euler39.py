6bde2509830b92b19c4b358f4c9a3cca0f65239b
Nikos Tsiodras <nikostsi230@gmail.com>
2021-03-14 15:03:38 +0200

    Very slow im gonna optimize it another time

---
+++ euler39.py
@@ -0,0 +1,18 @@
+def find_solutions(p):
+	product_lst = []
+	counter = 0
+	for a in range(1,p):
+		for b in range(a,p-a):
+			if a**2+b**2==(p-a-b)**2 and a*b not in product_lst:
+				# print(a,b,p-a-b)
+				product_lst.append(a*b)
+				counter+=1
+	return counter
+find_solutions(120)
+lst_p = []
+lst = []
+for p in range(10,1000):
+	print(p)
+	lst_p.append(p)
+	lst.append(find_solutions(p))
+print(lst_p[lst.index(max(lst))])
