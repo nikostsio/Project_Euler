a0cc8b69017a7d090c8fb89dc42d525068f4955a
Nikos Tsiodras <nikostsi230@gmail.com>
2021-03-14 10:10:51 +0200

    My solutions to the euler project

---
+++ euler34.py
@@ -0,0 +1,23 @@
+def factorial(n):
+	if n ==0:
+		return 1
+	if n==1:
+		return 1
+	return n*factorial(n-1)
+def check(n):
+	fact_sum = 0
+	for dig in str(n):
+		fact_sum += factorial(int(dig))
+	if fact_sum == n:
+		return True
+	return False
+
+def main():
+
+	my_sum = 0
+	for i in range(3,100000):
+		if check(i):
+			my_sum+=i
+	print(my_sum)
+if __name__=='__main__':
+	main()
